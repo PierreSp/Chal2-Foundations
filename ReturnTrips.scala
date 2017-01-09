049b414cf0af2fcbd8dd718aaa409c4085cfbe25
Nath <nathanael.bosch@gmail.com>
2017-01-09 12:53:37 +0100

    Basic Layouting of the code

--- ReturnTrips.scala
+++ ReturnTrips.scala
@@ -1,84 +1,116 @@
-
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.Column
-import org.apache.spark.sql.Dataset
-import org.apache.spark.sql.Row
-
-
+//Formula for exact distance computation via the great circle distance 
 def makeDistanceExpression(lat1 : Column, lat2 : Column, long1 : Column, long2 : Column) : Column = {
-val deltaphi = abs(lat1 - lat2)
-val deltalamda = abs(long1 - long2)
-val deltasigma = lit(2)*asin(sqrt(pow(sin(toRadians(deltaphi/lit(2))),2) + cos(toRadians(lat1))*cos(toRadians(lat2))*pow(sin(toRadians(deltalamda/lit(2))), 2)))        
-return deltasigma*lit(6371000)  //spark gibt automatisch das zurück was als letztes ausgewertet wird, dh. return nicht zwingend nötig
-}
-// As assume a earth radius of 6371 km , we want to use buckets of a size of about 105 m(this means we have 64000 buckets). Luckely we have deciaml coordinates
-// Thus 105 meters are  decimal degree. Thus we create two new columns, which are added ( withcolumns) to the main dataset.
 
-def setbucket_lat(lat : Column) : Column = {
-val latbucket = floor((lat + lit(180)) * lit(64000/360))
-return latbucket
+    val dLat=toRadians(lat1 - lat2)
+    val dLon=toRadians(long1 - long2)
+ 
+    val cterm = pow(sin(dLat/2),2) + pow(sin(dLon/2),2) * cos(toRadians(lat1)) * cos(toRadians(lat2))
+    val deltasigma = lit(2) * asin(sqrt(cterm))
+
+    return deltasigma*lit(6371e3)
 }
 
-def setbucket_long(long : Column) : Column = {
-val longbucket = floor((long + lit(180)) * lit(64000/360))
-return longbucket
+val tripsdist = trips.
+    withColumn("Distanceexact", makeDistanceExpression(
+        $"pickup_latitude",
+        $"dropoff_latitude",
+        $"pickup_longitude",
+        $"dropoff_longitude")).
+    withColumn("Pickuptime", $"tpep_pickup_datetime".cast("long")).
+    withColumn("Dropofftime", $"tpep_dropoff_datetime".cast("long"))
+
+//As stated in the description, we assume that the earth is a sphere with radius 6371km.
+//In order to find interessting points, we want to classify our points via their coordinates into buckets. 
+//Imagine we put a grid over the earth with quadrats of about 100m length use 63720
+val num_buckets = 63710 /360
+
+def initBucketLat(lat: Column) : Column = {
+    val BucketLat = floor((lat + lit(180)) * lit(num_buckets))
+    return BucketLat
 }
 
-
-object ReturnTrips {
-  def compute(trips : Dataset[Row], spark : SparkSession) : Dataset[Row] = {
-
-    import spark.implicits._
-
-    trips
-  }
+def initBucketLong(long: Column) : Column = {   
+    val BucketLong = floor((long + lit(180)) * lit(num_buckets))
+    return BucketLong
 }
 
-
-def setbucket_lat(lat : Column) : Column = {
-val latbucket = floor((lat + lit(180)) * lit(64000/360))
-return latbucket
+def initBucketTime(time: Column) : Column = {   
+    val BucketTime = floor((time.cast("long") / 28800D))
+    return BucketTime
 }
 
-def setbucket_long(long : Column) : Column = {
-val longbucket = floor((long + lit(180)) * lit(64000/360))
-return longbucket
-}
+val trips2 = trips.drop($"VendorID").drop($"passenger_count").drop($"trip_distance").drop($"RatecodeID").drop($"store_and_fwd_flag").drop($"payment_type").drop($"fare_amount").drop($"extra").drop($"mta_tax").drop($"tolls_amount").drop($"improvement_surcharge").drop($"total_amount").drop($"tip_amount")
 
-var tripbucket = trips.withColumn("LOBD", setbucket_long($"dropoff_longitude")).withColumn("LABD", setbucket_long($"dropoff_latitude")).withColumn("LOBP", setbucket_long($"pickup_longitude")).withColumn("LABP", setbucket_long($"pickup_latitude"))
+//Add the Bucket Columns to our Dataset
+val tripsBuckets = trips2.
+    withColumn("Pickup_Long_Bucket", initBucketLong($"pickup_longitude")).
+    withColumn("Pickup_Lat_Bucket", initBucketLat($"pickup_latitude")).
+    withColumn("Dropoff_Long_Bucket", initBucketLong($"dropoff_longitude")).
+    withColumn("Dropoff_Lat_Bucket", initBucketLat($"dropoff_latitude")).
+    withColumn("Pickup_Time_Bucket", initBucketTime($"tpep_pickup_datetime")).
+    withColumn("Dropoff_Time_Bucket", initBucketTime($"tpep_dropoff_datetime"))
 
-
-var tripbucket2 = trips.withColumn("LOBD", setbucket_long($"dropoff_longitude")).withColumn("LABD", setbucket_long($"dropoff_latitude")).withColumn("LOBP", setbucket_long($"pickup_longitude")).withColumn("LABP", setbucket_long($"pickup_latitude"))
-
-// SUPERBUCKET
-val superbuckettrip = tripbucket.union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"-1)).union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"-0)).union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"+1)).union(tripbucket.withColumn("LOBD",$"LOBD"+0).withColumn("LABD",$"LABD"-1)).union(tripbucket.withColumn("LOBD",$"LOBD"-0).withColumn("LABD",$"LABD"+1)).union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"-1)).union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"-0)).union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"+1))
- val superbuckettrip = tripbucket.union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"-1))
- .union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"-0))
- .union(tripbucket.withColumn("LOBD",$"LOBD"-1).withColumn("LABD",$"LABD"+1))
- .union(tripbucket.withColumn("LOBD",$"LOBD"+0).withColumn("LABD",$"LABD"-1))
- .union(tripbucket.withColumn("LOBD",$"LOBD"-0).withColumn("LABD",$"LABD"+1))
- .union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"-1))
- .union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"-0))
- .union(tripbucket.withColumn("LOBD",$"LOBD"+1).withColumn("LABD",$"LABD"+1))
-
-var joinedtest2 = superbuckettrip.joinWith(superbuckettrip2, superbuckettrip.col("LOBD")===superbuckettrip2.col("LOBP")) // JOIN
-
-
-val timesuperbuckettrip2 = timesuperbuckettrip
-
-var joinedtest2 = timesuperbuckettrip.joinWith(timesuperbuckettrip2, timesuperbuckettrip.col("LOBD")===timesuperbuckettrip2.col("LOBP") &&  timesuperbuckettrip.col("LABD")===timesuperbuckettrip2.col("LABP")) // JOIN
+//We now multiply our dataset 9 times to get all the neighbour buckets (because there might be interessting points in neighbour buckets as well)
+val tripsClones = tripsBuckets.
+    withColumn(
+        "Pickup_Long_Bucket", 
+        explode(array($"Pickup_Long_Bucket"-1, $"Pickup_Long_Bucket", $"Pickup_Long_Bucket"+1))).
+    unionAll(
+        tripsBuckets.withColumn(
+            "Pickup_Lat_Bucket", 
+            explode(array($"Pickup_Lat_Bucket"-1, $"Pickup_Lat_Bucket", $"Pickup_Lat_Bucket"+1)))).
+    unionAll(
+        tripsBuckets.withColumn(
+            "Pickup_Time_Bucket", 
+            explode(array($"Pickup_Time_Bucket", $"Pickup_Time_Bucket"-1))))
 
 
+//Join with trips where buckets of pickup and dropoff are the same
+val JoinedTrips = tripsClones.as("to").
+    join(
+        tripsBuckets.as("back"), 
+        $"to.Pickup_Long_Bucket" === $"back.Dropoff_Long_Bucket" &&
+        $"to.Pickup_Lat_Bucket" === $"back.Dropoff_Lat_Bucket" &&
+        $"to.Dropoff_Time_Bucket" === $"back.Pickup_Time_Bucket" &&
+        $"back.Pickup_Long_Bucket" === $"to.Dropoff_Long_Bucket" &&
+        $"back.Pickup_Lat_Bucket" === $"to.Dropoff_Lat_Bucket", "inner")
 
-var wjoin = superbuckettrip.join(tripbucket, superbuckettrip.col("LOBD")===tripbucket.col("LOBP") && superbuckettrip.col("LABD")===tripbucket.col("LABP"),"left") // JOIN
+val Joinedfilter = JoinedTrips.
+    drop($"to.Pickup_Long_Bucket").
+    drop($"to.Pickup_Lat_Bucket").
+    drop($"to.Dropoff_Long_Bucket").
+    drop($"to.Dropoff_Lat_Bucket").
+    drop($"to.Pickup_Time_Bucket").
+    drop($"to.Dropoff_Time_Bucket").
+    drop($"back.Pickup_Long_Bucket").
+    drop($"back.Pickup_Lat_Bucket").
+    drop($"back.Dropoff_Long_Bucket").
+    drop($"back.Dropoff_Lat_Bucket").
+    drop($"back.Pickup_Time_Bucket").
+    drop($"back.Dropoff_Time_Bucket").
+    drop($"back.tpep_dropoff_datetime").
+    drop($"to.tpep_pickup_datetime")
 
-val timedstuff = wjoin.where( _1$"tpep_dropoff_datetime".cast("long") < _2$"tpep_pickup_datetime".cast("long")).where((( _1$"tpep_dropoff_datetime".cast("long")-_2$"tpep_pickup_datetime".cast("long")) / 3600D)>8)// ZEIT FILER
+val exactJoinedTripsTime = Joinedfilter.
+    where((($"to.tpep_dropoff_datetime").cast("long")) < (($"back.tpep_pickup_datetime").cast("long")))
 
+val JoinedUnique = exactJoinedTripsTime.dropDuplicates
 
-var exactdist = wjoin.where(makeDistanceExpression(_1$"pickup_latitude",_2$"dropoff_latitude",_1$"pickup_longitude",_2$"dropoff_longitude")).where(makeDistanceExpression(_2$"pickup_latitude",_1$"dropoff_latitude",_2$"pickup_longitude",_1$"dropoff_longitude"))
-trips.select((col("tpep_dropoff_datetime").cast("long")-col("tpep_pickup_datetime").cast("long")) / 3600D).show
+//Now we need to check the hour rxestringation and only return those where b's pickup time is within 8 hours after a's dropoff time.
+val exactJoinedTripsTime2 = JoinedUnique.
+    where((($"to.tpep_dropoff_datetime").cast("long")) + 28800D > (($"back.tpep_pickup_datetime").cast("long")))
+
+//For those trips where buckets are +/-1 equal, we calculate the exact distance and only consider those where the distance really is smaller or equal to 100m.
+val exactJoinedTrips = exactJoinedTripsTime.
+    where(
+        makeDistanceExpression(
+            $"back.pickup_latitude", 
+            $"to.dropoff_latitude", 
+            $"back.pickup_longitude", 
+            $"to.dropoff_longitude") <= 100).
+    where(
+        makeDistanceExpression(
+            $"to.pickup_latitude", 
+            $"back.dropoff_latitude", 
+            $"to.pickup_longitude", 
+            $"back.dropoff_longitude") <= 100)
